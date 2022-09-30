c0600fe0bdde06d8fd25c075bcc9270670f620f0
a-vachkov <108079896+a-vachkov@users.noreply.github.com>
2022-09-30 03:28:04 +0300

    D7 is done

---
+++ newsportal/news_portal_app/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.urls import reverse
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.SmallIntegerField(default=0)
+
+    def update_ratings(self):
+        posts_rating = self.post_set.aggregate(result=Sum('rating')).get('result')
+        comments_rating = self.user.comment_set.aggregate(result=Sum('rating')).get('result')
+        print(f"===== {self.user}: обновляем рейтинг автора =====")
+        print(f"Рейтинг постов = {posts_rating}")
+        print(f"Рейтинг комментов = {comments_rating}")
+        self.rating = 3 * posts_rating + comments_rating
+        self.save()
+        print(f"Рейтинг = 3 * {posts_rating} + {comments_rating} = {self.rating}")
+
+
+class Category(models.Model):
+    name_category = models.CharField(max_length=128, unique=True)
+
+
+class Post(models.Model):
+    news = 'NE'
+    artikle = 'AR'
+
+    TYPE = [
+        (news, 'Новость'),
+        (artikle, 'Статья'),
+    ]
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type = models.CharField(max_length=2, choices=TYPE, default=artikle)
+    time = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self, length=124):
+        return f"{self.text[:length]}..." if len(str(self.text)) > length else self.text
+
+    def get_absolute_url(self):
+        """ Вернуть url, зарегистрированный для отображения одиночного товара """
+        return reverse('post_detail', args=[str(self.id)])
+
+
+class PostCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating = + 1
+        self.save()
+
+    def dislike(self):
+        self.rating = - 1
+        self.save()
---
+++ newsportal/news_portal_app/views.py
@@ -0,0 +1,109 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Author
+from .filters import PostFilter
+from .forms import PostForm
+
+
+class PostsList(ListView):
+    paginate_by = 10
+    model = Post
+    ordering = 'time'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+#class SearchAuthor(ListView):
+#    model = Author
+
+
+class SearchPosts(ListView):
+    paginate_by = 10
+    model = Post
+    ordering = 'time'
+    template_name = 'search.html'
+    context_object_name = 'news'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_filter'] = self.filterset
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class ArticleCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Добавить статью"
+        return context
+
+
+class ArticleUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Редактировать статью"
+        return context
+
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts_list')
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Удалить статью"
+        context['previous_page_url'] = reverse_lazy('posts_list')
+        return context
+
+
+class NewsCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Добавить новость"
+        return context
+
+
+class NewsUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Редактировать новость"
+        return context
+
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts_list')
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Удалить новость"
+        context['previous_page_url'] = reverse_lazy('posts_list')
+        return context
